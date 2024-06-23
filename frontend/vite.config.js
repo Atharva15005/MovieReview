b4488474cbbda7e3693a6dfcc3358daa71000d6f
Atharva Deshmukh <athruydeshmukh526m@gmail.com>
2024-06-23 18:30:06 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // or your desired hostname
+    port: 3000, // or your desired port
+  },
 })
